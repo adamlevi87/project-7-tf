894b833cfc940c98f7be7c2d50b96c818d9563ab
Adam Levi <adamlevi87@gmail.com>
2025-09-17 20:02:14 +0300

    added auto accept true

--- terraform-runner-infra/modules/vpc_peering/main.tf
+++ terraform-runner-infra/modules/vpc_peering/main.tf
@@ -62,7 +62,7 @@ resource "aws_vpc_peering_connection" "to_main" {
   vpc_id      = var.source_vpc_id
   peer_vpc_id = try(data.terraform_remote_state.main.outputs.main_vpc_info.vpc_id, "fake-id")  # fake-id Prevents validation error
   peer_region = try(data.terraform_remote_state.main.outputs.main_vpc_info.region, "fake-region") # fake-region fake region that will never be applied
-  auto_accept = false  # Changed to true - will remove the module from the other TF job
+  auto_accept = true  # Changed to true - will remove the module from the other TF job
 
   requester {
     allow_remote_vpc_dns_resolution = false
