3af8c18a20a70d109ed0ae0e5d8d6d922c0a6c72
Adam Levi <adamlevi87@gmail.com>
2025-09-17 15:01:29 +0300

    reverted back explicitly set requester and accepter

--- terraform-runner-infra/modules/vpc_peering/main.tf
+++ terraform-runner-infra/modules/vpc_peering/main.tf
@@ -61,8 +61,16 @@ resource "aws_vpc_peering_connection" "to_main" {
 
   vpc_id      = var.source_vpc_id
   peer_vpc_id = try(data.terraform_remote_state.main.outputs.main_vpc_info.vpc_id, "fake-id")  # fake-id Prevents validation error
-  #peer_region = try(data.terraform_remote_state.main.outputs.main_vpc_info.region, "fake-region") # fake-region fake region that will never be applied
-  auto_accept = true  # Changed to true - will remove the module from the other TF job
+  peer_region = try(data.terraform_remote_state.main.outputs.main_vpc_info.region, "fake-region") # fake-region fake region that will never be applied
+  auto_accept = false  # Changed to true - will remove the module from the other TF job
+
+  requester {
+    allow_remote_vpc_dns_resolution = false
+  }
+
+  accepter {
+    allow_remote_vpc_dns_resolution = false
+  }
 
   tags = {
     Name        = "${var.project_tag}-${var.environment}-to-main-peering"
@@ -75,15 +83,15 @@ resource "aws_vpc_peering_connection" "to_main" {
   depends_on = [null_resource.validate_outputs_or_fail]
 }
 
-resource "aws_vpc_peering_connection_options" "to_main" {
-  vpc_peering_connection_id = aws_vpc_peering_connection.to_main.id
+# resource "aws_vpc_peering_connection_options" "to_main" {
+#   vpc_peering_connection_id = aws_vpc_peering_connection.to_main.id
 
-  accepter {
-    allow_remote_vpc_dns_resolution = false
-  }
+#   accepter {
+#     allow_remote_vpc_dns_resolution = false
+#   }
 
-  depends_on = [ aws_vpc_peering_connection.to_main ]
-}
+#   depends_on = [ aws_vpc_peering_connection.to_main ]
+# }
 
 # Add route to main VPC through peering connection
 resource "aws_route" "runner_to_main" {
